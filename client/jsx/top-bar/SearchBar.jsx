709292e12823e5daa7a15cacbd048a2cc985f649
luke <lukes611@gmail.com>
2017-04-19 09:26:05 +1000

    working on top-bar more

--- client/jsx/top-bar/SearchBar.jsx
+++ client/jsx/top-bar/SearchBar.jsx
@@ -1,24 +1,38 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {entry : ''};
+  }
+
   render(){
+
+    let drawGuesses = this.state.entry.length > 0;
+    let guesses = 'hello,tonka,blue,bird'.split(',');
+
     return (
       <div className="top-bar-2">
         <div className="top-bar-2-sub">
-          <input type="text" className="top-bar-search-bar" placeholder="Search..."></input>
-          <div className="top-bar-search-guess">
-            blah...<br/>
-          blah...<br/>
-        blah...<br/>
-      blah...<br/>
-    blah...<br/>
-  blah...<br/>
-blah...<br/>
+          <input type="text" className="top-bar-search-bar" placeholder="Search..." onChange={this.updateEntry.bind(this)}></input>
+          <button className="top-bar-search-button">
+            {/*<img src="ims/search-icon.png"></img>*/}
+          </button>
+          <div className="top-bar-search-guess" style={{display : drawGuesses ? 'block' : 'none'}}>
+            {guesses.map((g,i) => <div key={i}>{g}</div>)}
           </div>
         </div>
       </div>
     );
   }
+
+  updateEntry(e){
+    let value = e.target.value;
+    this.setState({entry : value});
+  }
+
 }
+
 
 export default SearchBar;
