7ffaeac652bbae0145f0b5a7c298d5c0cf28b497
lukes611 <lukes611@gmail.com>
2017-04-27 13:23:45 +1000

    saving

--- client/jsx/sign-in.jsx
+++ client/jsx/sign-in.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class TopBanner extends React.Component{
+  render(){
+    let goHome = () => {
+      window.location.href = '/';
+    };
+
+    return (
+      <div className="sign-in-top-bar">
+        <img src="ims/logo.png" onClick={goHome}></img>
+      </div>
+    );
+  }
+}
+
 class LogIn extends React.Component{
   render(){
+
+    if(!this.props.display) return null;
+
     return (
-      <div>
-        log-in stuff
+      <div className="log-in-body">
+        username : <br/>
+        <input type="text" /> <br/>
+        password : <br/>
+        <input type="password" /> <br/>
+        remember me: <input type="checkbox" />
       </div>
     );
   }
@@ -13,8 +34,11 @@ class LogIn extends React.Component{
 
 class SignUp extends React.Component{
   render(){
+
+    if(!this.props.display) return null;
+
     return (
-      <div>
+      <div className="sign-up-body">
         sign-up stuff
       </div>
     );
@@ -25,24 +49,38 @@ class SignUp extends React.Component{
 class App extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      signInOn : true
+    };
   }
 
   render(){
+
+    let signInOn = this.state.signInOn;
+    let unselectedStyle = {
+      backgroundColor : 'rgb(41,128,185)',
+      color : 'rgb(236,240,241)'
+    };
+
     return (
-      <div style={{backgroundColor:'red'}}>
-        <div style={{backgroundColor:'blue'}}>
-          <img src="ims/logo.png"></img>
-        </div>
-        <div style={{backgroundColor:'green'}}>
-          <div style={{backgroundColor:'orange'}}>
-            <div style={{backgroundColor:'red'}}>log in</div>
-            <div style={{backgroundColor:'lightblue'}}>sign up</div>
-            <LogIn/>
-            <SignUp/>
+      <div className="sign-in-app">
+        <TopBanner/>
+        <div className="sign-in-body">
+          <div className="sign-in-contents">
+            <div className="sign-in-form">
+              <div className="sign-in-loginButton" style={signInOn?{}:unselectedStyle}  onClick={this.signInOn.bind(this, true)} >log-in</div>
+              <div className="sign-in-signUpButton" style={!signInOn?{}:unselectedStyle} onClick={this.signInOn.bind(this, false)}>sign up</div>
+                <LogIn display={signInOn} />
+                <SignUp display={!signInOn} />
+            </div>
           </div>
         </div>
       </div>
     );
+  }
+
+  signInOn(on){
+    this.setState({signInOn : on});
   }
 
 }
