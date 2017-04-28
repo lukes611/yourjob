b98a7114765f20f39b0d28ea584779ab7f8de63a
luke <lukes611@gmail.com>
2017-04-28 16:40:30 +1000

    adding cleanup functionality

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 //server by luke lincoln
 var ContentRoutes = require('./server/ContentRoutes.js');
+var Cleanup = require('./server/Cleanup.js');
 var express = require('express');
 var fs = require('fs');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('test.db');
+;
 
-var cleanup = (function(){
-  if(this.clean) return;
-  console.log('\nclosing database');
+Cleanup(function(){
+  console.log('\nshutting down...');
   db.close();
-  this.done();
-}).bind({clean : false, done : function(){ this.clean=true; process.exit()}});
-
-//do something when app is closing
-process.on('exit', cleanup);
-//catches ctrl+c event
-process.on('SIGINT', cleanup);
-//catches uncaught exceptions
-process.on('uncaughtException', cleanup);
+});
 
 //add in table if not exists
 /*
---
+++ server/Cleanup.js
@@ -0,0 +1,24 @@
+
+var func = function(){
+  console.log('program complete...');
+};
+
+module.exports = function(cb){
+
+  func = cb;
+
+  var cleanup = (function(){
+    if(this.clean) return;
+    cb();
+    this.done();
+  }).bind({clean : false, done : function(){ this.clean=true; process.exit()}});
+
+  //do something when app is closing
+  process.on('exit', cleanup);
+  //catches ctrl+c event
+  process.on('SIGINT', cleanup);
+  //catches uncaught exceptions
+  process.on('uncaughtException', cleanup);
+
+
+};
